9e2d76eeda6794a0e6bc6052608240a9f9dccd4a
Mark Lessard <nlessard@mon-avenir.ca>
2023-05-31 13:51:15 -0400

    Update asdfsadff.rb

--- tasks/asdfsadff.rb
+++ tasks/asdfsadff.rb
@@ -1,1 +1,105 @@
-asdfsadff
+class bell_part < UrRobot
+    include Turner
+end
+
+def bell_part
+move; 
+place_beeper; 
+move; 
+place_beeper; 
+move; 
+place_beeper; 
+move; 
+place_beeper; 
+move; 
+place_beeper; 
+move; 
+place_beeper;
+move; 
+place_beeper;
+move;
+end
+
+def run_task
+    bell_part
+  end
+
+class bell_part2 < UrRobot
+    include Turner
+end
+
+def bell_part2
+move; 
+place_beeper; 
+move; 
+place_beeper; 
+move; 
+place_beeper; 
+move; 
+place_beeper; 
+move; 
+place_beeper; 
+move;
+end
+
+def run_task
+    bell_part2
+  end
+
+class bell_part3 < UrRobot
+    include Turner
+end
+
+def bell_part3
+move; 
+place_beeper; 
+move; 
+place_beeper; 
+move; 
+place_beeper;
+turn_right
+move
+turn_right
+move; 
+place_beeper; 
+move; 
+place_beeper; 
+move; 
+place_beeper;
+turn_left
+move
+turn_left
+end
+
+def run_task
+    bell_part3
+  end
+           Code final
+move
+place_beeper
+turn_right
+move; move; move;
+turn_left
+move
+turn_left
+def run_task
+    bell_part
+  end
+turn_right
+move
+turn_right
+def run_task
+    bell_part2
+  end
+turn_left
+move
+turn_left
+run_task
+def run_task
+    bell_part3
+  end
+def run_task
+    bell_part3
+  end
+move
+place_beeper
